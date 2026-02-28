0d42b6bd4c06c3151eaef68548e68f9704f9b088
domeniksabitzer08-create <domeniksabitzer08@gmail.com>
2026-02-28 16:07:38 +0100

    tried to train agent

--- src/Q_learning.py
+++ src/Q_learning.py
@@ -1,5 +1,8 @@
+import time
 from dataclasses import dataclass
 import random
+from time import sleep
+
 import numpy as np
 import pygame
 from torch.distributed.argparse_util import env
@@ -24,11 +27,11 @@ class ENV:
         self.start_pos = start_pos
         self.reward_pos_grid = reward_pos_grid
         self.blocks = blocks
-        self.agent_start_pos = Vector2(0,0)
+        self.agent_start_pos = Vector2(0,2)
         self.state = self.agent_start_pos
 
     def step(self, action) -> (Vector2, int, bool):
-        reward = -1
+        reward = 0
         is_done = False
         # Move if there is no border or wall
         direction_lut = [
@@ -54,14 +57,14 @@ class ENV:
                 next_state = self.state
         # Check if agent reacher target
         if next_state == self.reward_pos_grid:
-            reward = 0
+            reward = 1
             is_done = True
         self.state = next_state
-        return next_state, reward, is_done
+        return (next_state.x*next_state.y), reward, is_done
 
-    def reset(self) -> Vector2:
+    def reset(self):
         self.state = self.agent_start_pos
-        return self.state
+        return (self.state.x*self.state.y)
     def sample(self):
         """returns a random action"""
         action = [0,0,0,0]
@@ -70,7 +73,8 @@ class ENV:
         return action
 
 
-    def render(self):
+    def render(self, episodes, n_finished):
+        screen.fill((0, 0, 0))
         # render vertical lines
         for i in range(self.cell_count_x+1):
             line_pos_ver = Vector2(self.start_pos.x + i * self.cell_width, self.start_pos.y)
@@ -84,6 +88,16 @@ class ENV:
         # Render blocks
         for pos in self.blocks:
             self.render_cell(pos, (100,100,100))
+        # Render agent
+        self.render_cell(self.state, (0,0,255))
+        self.render_ui(episodes, n_finished)
+        pygame.display.update()
+
+    def render_ui(self, epsilons, n_finished):
+        font = pygame.font.SysFont("Comic Sans MS", 20)
+        label = font.render(f"Epsilons: {epsilons} | finished: {n_finished}", 1, (230,230,255))
+        self.screen.blit(label, (50, 300))
+
 
     def render_line_hor(self, pos: Vector2):
         line = pygame.Rect(pos.x, pos.y , self.cell_width * self.cell_count_x + self.line_thickness, self.line_thickness)
@@ -98,8 +112,10 @@ class ENV:
         rect = pygame.Rect(pos.x + self.line_thickness , pos.y + self.line_thickness, self.cell_width - self.line_thickness , self.cell_width - self.line_thickness)
         pygame.draw.rect(self.screen, color, rect)
 
+
     def grid_to_screen_pos(self, pos: Vector2):
         return Vector2(pos.x * self.cell_width + self.start_pos.x , pos.y * self.cell_width + self.start_pos.y)
+
 
 ### MAIN PROGRAM STARTS HERE ###
 pygame.init()
@@ -107,42 +123,57 @@ screen = pygame.display.set_mode((500, 500))
 # Grid setup
 cell_count_x, cell_count_y = 5, 3
 reward_pos = Vector2(4,2) # in grid coordinates
-blocks = [Vector2(2,2), Vector2(2,1)]
+#blocks = [Vector2(2,2), Vector2(2,1)]
+blocks = []
 Env = ENV(cell_width=50, cell_count_x=cell_count_x, cell_count_y=cell_count_y, start_pos= Vector2(50, 100), line_thickness=4, screen=screen, reward_pos_grid=reward_pos, blocks=blocks)
 # Q-Table
-q_table = np.zeros((cell_count_x, cell_count_y))
+q_table = np.zeros((cell_count_x*cell_count_y, 4))
 # Values
 lr = 0.1 # Learning rate
-gamma = 0.95 # value of future rewards
-epsilon = 1 # exploration (random action) factor
-epsilon_decay = 0.95
-num_episodes = 1000
+gamma = 0.99 # value of future rewards
+epsilon = 5 # exploration (random action) factor
+min_epsilon = 0.1
+epsilon_decay = 0.99
+num_episodes = 10000
 max_steps = 20
+n_finished = 0
 
 def choose_action(state):
     if random.uniform(0,1) <= epsilon:
         return Env.sample()
     else:
-        return np.argmax(q_table[state])
+        return np.argmax(q_table[state,:])
 
 while True:
     for i in range(num_episodes):
+        #print(f"Episode: {i} | end state {Env.state}")
         is_done = False
         state = Env.reset()
         for j in range(max_steps):
+
+            # Rendering
+            Env.render(i,n_finished)
+
+
             action = choose_action(state)
             next_state, reward, is_done = Env.step(action)
-
             old_value = q_table[state,action]
             next_max = np.max(q_table[next_state])
 
             q_table[state,action] = lr * old_value + (1-lr) * (reward + gamma * next_max)
 
+            state = next_state
+            if is_done:
+                n_finished += 1
+                break
+
+        epsilon = max(min_epsilon, epsilon*epsilon_decay)
 
 
 
-    Env.render()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print(f"Q-TABLE: {q_table}")
             pygame.quit()
-    pygame.display.update()
+
