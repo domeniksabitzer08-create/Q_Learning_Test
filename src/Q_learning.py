55f08b6b575468c1fdab81fd38736fdf0a4761e4
domeniksabitzer08-create <domeniksabitzer08@gmail.com>
2026-03-01 16:45:06 +0100

    fixed all bugs, agent now learning correctly, added bigger maze

--- src/Q_learning.py
+++ src/Q_learning.py
@@ -27,7 +27,7 @@ class ENV:
         self.start_pos = start_pos
         self.reward_pos_grid = reward_pos_grid
         self.blocks = blocks
-        self.agent_start_pos = Vector2(0,2)
+        self.agent_start_pos = Vector2(0,0)
         self.state = self.agent_start_pos
 
     def step(self, action) -> (int, float, bool):
@@ -63,7 +63,7 @@ class ENV:
             is_done = True
             self.state = next_state
         self.state = next_state
-        return (next_state.x*next_state.y), reward, is_done
+        return (next_state.y * cell_count_x + next_state.x), reward, is_done
 
     def reset(self):
         self.state = self.agent_start_pos
@@ -74,7 +74,7 @@ class ENV:
         return action
 
 
-    def render(self, episodes, n_finished, epsilon):
+    def render(self, episodes, n_finished, epsilon, step):
         screen.fill((0, 0, 0))
         # render vertical lines
         for i in range(self.cell_count_x+1):
@@ -91,16 +91,16 @@ class ENV:
             self.render_cell(pos, (100,100,100))
         # Render agent
         self.render_cell(self.state, (0,0,255))
-        self.render_ui(episodes, n_finished, epsilon)
+        self.render_ui(episodes, n_finished, epsilon, step)
         pygame.display.update()
 
-    def render_ui(self, episodes, n_finished, epsilon):
+    def render_ui(self, episodes, n_finished, epsilon, step):
         font = pygame.font.SysFont("Comic Sans MS", 20)
-        acc = n_finished/episodes *100
+        acc = n_finished/(episodes+1) *100
         label = font.render(f"Episodes: {episodes} | finished: {n_finished} | epsilon: {epsilon:.2f}", 1, (230,230,255))
-        label2 = font.render(f"accuracy: {acc:.2f}%", 1, (230,230,255))
-        self.screen.blit(label, (50, 300))
-        self.screen.blit(label2, (50, 330))
+        label2 = font.render(f"accuracy: {acc:.2f}% | steps: {step}", 1, (230,230,255))
+        self.screen.blit(label, (50, 330+20))
+        self.screen.blit(label2, (50, 350+20))
 
 
     def render_line_hor(self, pos: Vector2):
@@ -125,29 +125,37 @@ class ENV:
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 # Grid setup
-cell_count_x, cell_count_y = 5, 3
-reward_pos = Vector2(4,2) # in grid coordinates
-blocks = [Vector2(2,2), Vector2(2,1)]
-#blocks = []
+cell_count_x, cell_count_y = 6, 5
+reward_pos = Vector2(5,4) # in grid coordinates
+# Maze #
+empty_maze = blocks = []
+
+easy_maze = [Vector2(2,2), Vector2(2,1)] # start pos: 0,2 | reward_pos: 4, 2 | cell count 5,3
+mid_maze = [Vector2(2,0), Vector2(2,1), Vector2(2,2), Vector2(2,3), Vector2(4,1), Vector2(4,2), Vector2(4,3), Vector2(4,4)] # start pos: 0,0 | reward_pos: 5, 4 | cell count 6,5
+
+blocks = mid_maze
+
 Env = ENV(cell_width=50, cell_count_x=cell_count_x, cell_count_y=cell_count_y, start_pos= Vector2(50, 100), line_thickness=4, screen=screen, reward_pos_grid=reward_pos, blocks=blocks)
 # Q-Table
 q_table = np.zeros((cell_count_x*cell_count_y, 4))
 # Values
 lr = 0.1 # Learning rate
 gamma = 0.95 # value of future rewards
-epsilon = 10 # exploration (random action) factor
-min_epsilon = 0.05
+epsilon = 1 # exploration (random action) factor
+min_epsilon = 0.1
 epsilon_decay = 0.995
-num_episodes = 100000
+num_episodes = 10000
 max_steps = 20
-n_finished = 0
-new_n_finished = 0
+train_n_finished = 0
+test_n_finished = 0
+
 def choose_action(state):
     if random.uniform(0,1) <= epsilon:
         return Env.sample()
     else:
         return np.argmax(q_table[state,:])
 
+### Training ###
 
 for episode in tqdm(range(num_episodes)):
     #if episode % 10000 == 0:
@@ -157,30 +165,39 @@ for episode in tqdm(range(num_episodes)):
     for j in range(max_steps):
 
         # Rendering
-        #Env.render(episode, n_finished, epsilon)
-        if episode == 90000:
-            min_epsilon = 0
-            new_num_episodes = num_episodes - 90000
-            new_n_finished = 0
+        #Env.render(episode, train_n_finished, epsilon, j)
         action = choose_action(state)
         next_state, reward, is_done = Env.step(action)
         old_value = q_table[state,action]
         next_max = np.max(q_table[next_state])
 
-        q_table[state,action] = lr * old_value + (1-lr) * (reward + gamma * next_max)
+        q_table[state,action] = old_value + lr *(reward + gamma * next_max ) - old_value
 
         state = next_state
         if is_done:
-            n_finished += 1
-            new_n_finished += 1
+            train_n_finished += 1
             break
 
     epsilon = max(min_epsilon, epsilon*epsilon_decay)
-acc = (new_n_finished/new_num_episodes)*100
-print("left -------- up -------- right -------- down")
-print(f"{q_table}")
-print(f"\n accuracy: {acc:.2f}% | finished: {new_n_finished} | episodes: {new_num_episodes}")
+acc = (train_n_finished / num_episodes) * 100
+print(f"\n accuracy: {acc:.2f}% | finished: {train_n_finished} | episodes: {num_episodes}")
 
+### Testing ###
+test_episodes = 100
+
+for episode in range(test_episodes):
+    is_done = False
+    state = Env.reset()
+    for step in range(max_steps):
+        # Rendering
+        Env.render(episode, test_n_finished, epsilon, step)
+        sleep(0.1)
+        action = choose_action(state)
+        next_state, reward, is_done = Env.step(action)
+        state = next_state
+        if is_done:
+            test_n_finished += 1
+            break
 
 
 
